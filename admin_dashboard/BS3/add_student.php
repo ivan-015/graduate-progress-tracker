4bdb83b6ffc7f9c80e863eea4a99a2bc45454c5c
gabrielM01u <55124163+gabrielM01u@users.noreply.github.com>
2020-05-06 23:40:43 -0600

    removed faulty error handling

--- admin_dashboard/BS3/add_student.php
+++ admin_dashboard/BS3/add_student.php
@@ -6,7 +6,7 @@ require_once "config.php";
 
 /* Empty values for general user */
 $password = $id = $email = $f_name = $l_name = $m_name = "";
-$username_err = $password_err = "";
+
 
 /* Empty values for student user */
  $major_gpa = $is_doctorate = $credit_hours = $funding = $graduate_date = $date_of_entry = $gpa = '';
@@ -93,61 +93,61 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <h2>Add Student</h2>
         <p>Please fill this form to create an student account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+
                 <label>Emai</label>
                 <input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
+
             </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group ">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
+
             </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group">
                 <label>First Name</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $f_name; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
+
             </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group">
                 <label>Last Name</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $l_name; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
+
             </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group">
                 <label>Middle Name</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $m_name; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
+
             </div>
             <br>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group">
                 <label>Major GPA</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $major_gpa; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
+
             </div>
             <div class="form-group">
                 <label>Credit Hours</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $credit_hours; ?>">
-                <span class="help-block"></span>
+
             </div>
             <div class="form-group">
                 <label>Funding</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $funding; ?>">
-                <span class="help-block"></span>
+
             </div>
             <div class="form-group">
                 <label>Graduate Date</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $graduate_date; ?>">
-                <span class="help-block"></span>
+
             </div>
             <div class="form-group">
                 <label>Date Of Entry</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $date_of_entry; ?>">
-                <span class="help-block"></span>
+
             </div>
             <div class="form-group">
                 <label>GPA</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $gpa; ?>">
-                <span class="help-block"></span>
+
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
